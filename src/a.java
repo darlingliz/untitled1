b399c92012eed44a1d24a1b35c8914ee9436322c
liz <darlingliz303@gmail.com>
2023-10-10 21:19:06 +0800

    commit third

--- src/a.java
+++ src/a.java
@@ -2,6 +2,7 @@ public class a {
     public static void main(String[]args){
         System.out.println("hello");
         System.out.println("hello1");
+        System.out.println("hello2");
 
     }
 }
