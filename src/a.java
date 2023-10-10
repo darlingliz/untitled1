f1cea55a367874cc9921f2774dbbb0a730adfbe5
liz <darlingliz303@gmail.com>
2023-10-10 21:28:45 +0800

    commit hot-fix second

--- src/a.java
+++ src/a.java
@@ -4,7 +4,7 @@ public class a {
         System.out.println("hello1");
         System.out.println("hello2");
         System.out.println("hello3 hot-fix");
-
+        System.out.println("hello4 hot-fix second");
 
     }
 }
